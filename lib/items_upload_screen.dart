08cca123cc01e65e1ecf7f692726c1adb6a5c9e3
Ruchita613 <ruchita.sawale@getpeppermint.co>
2023-11-03 22:05:55 +0530

    upload form screen done

--- lib/items_upload_screen.dart
+++ lib/items_upload_screen.dart
@@ -11,6 +11,17 @@ class ItemsUploadScreen extends StatefulWidget {
 
 class _ItemsUploadScreenState extends State<ItemsUploadScreen> {
   Uint8List? imageFileUint8List;
+
+  TextEditingController sellerNameTextEditingController =
+      TextEditingController();
+  TextEditingController sellerPhoneTextEditingController =
+      TextEditingController();
+  TextEditingController itemNameTextEditingController = TextEditingController();
+  TextEditingController itemDescriptionTextEditingController =
+      TextEditingController();
+  TextEditingController itemPriceTextEditingController =
+      TextEditingController();
+
   bool isuploading = false;
 
   //upload form screen
@@ -22,6 +33,14 @@ class _ItemsUploadScreenState extends State<ItemsUploadScreen> {
           "Upload Items",
           style: TextStyle(color: Colors.white),
         ),
+        actions: [
+          IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.cloud_done,
+                color: Colors.white,
+              ))
+        ],
         centerTitle: true,
       ),
       body: ListView(
@@ -42,7 +61,128 @@ class _ItemsUploadScreenState extends State<ItemsUploadScreen> {
                         Icons.image_not_supported,
                         color: Colors.grey,
                       )),
-          )
+          ),
+          const Divider(
+            color: Colors.white,
+            thickness: 2,
+          ),
+
+          //seller name list tile -------------------------------------------------------------
+
+          ListTile(
+            leading: const Icon(
+              Icons.person_pin_rounded,
+              color: Colors.white,
+            ),
+            title: SizedBox(
+              width: 250,
+              child: TextField(
+                style: const TextStyle(color: Colors.white),
+                controller: sellerNameTextEditingController,
+                decoration: const InputDecoration(
+                    hintText: "seller name",
+                    hintStyle: TextStyle(color: Colors.green),
+                    border: InputBorder.none),
+              ),
+            ),
+          ),
+          const Divider(
+            color: Colors.white30,
+            thickness: 1,
+          ),
+
+          // seller phone
+          ListTile(
+            leading: const Icon(
+              Icons.person_pin_rounded,
+              color: Colors.white,
+            ),
+            title: SizedBox(
+              width: 250,
+              child: TextField(
+                style: const TextStyle(color: Colors.white),
+                controller: sellerPhoneTextEditingController,
+                decoration: const InputDecoration(
+                    hintText: "seller Phone",
+                    hintStyle: TextStyle(color: Colors.green),
+                    border: InputBorder.none),
+              ),
+            ),
+          ),
+          const Divider(
+            color: Colors.white30,
+            thickness: 1,
+          ),
+
+          // Item Name
+          ListTile(
+            leading: const Icon(
+              Icons.person_pin_rounded,
+              color: Colors.white,
+            ),
+            title: SizedBox(
+              width: 250,
+              child: TextField(
+                style: const TextStyle(color: Colors.white),
+                controller: itemNameTextEditingController,
+                decoration: const InputDecoration(
+                    hintText: "item name",
+                    hintStyle: TextStyle(color: Colors.green),
+                    border: InputBorder.none),
+              ),
+            ),
+          ),
+          const Divider(
+            color: Colors.white30,
+            thickness: 1,
+          ),
+
+          // Item Description
+          ListTile(
+            leading: const Icon(
+              Icons.person_pin_rounded,
+              color: Colors.white,
+            ),
+            title: SizedBox(
+              width: 250,
+              child: TextField(
+                style: const TextStyle(color: Colors.white),
+                controller: itemDescriptionTextEditingController,
+                decoration: const InputDecoration(
+                    hintText: "Item description ",
+                    hintStyle: TextStyle(color: Colors.green),
+                    border: InputBorder.none),
+              ),
+            ),
+          ),
+          const Divider(
+            color: Colors.white30,
+            thickness: 1,
+          ),
+
+          // item price
+
+          ListTile(
+            leading: const Icon(
+              Icons.person_pin_rounded,
+              color: Colors.white,
+            ),
+            title: SizedBox(
+              width: 250,
+              child: TextField(
+                style: const TextStyle(color: Colors.white),
+                controller: itemPriceTextEditingController,
+                decoration: const InputDecoration(
+                    hintText: "Item Price ",
+                    hintStyle: TextStyle(color: Colors.green),
+                    border: InputBorder.none),
+              ),
+            ),
+          ),
+          const Divider(
+            color: Colors.white30,
+            thickness: 1,
+          ),
         ],
       ),
     );
